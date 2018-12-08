d9a7276967da442c55cb745c7fddac7f50272667
Maryre <en.mrezaei@gmail.com>
2018-12-08 18:59:50 +0330

    creat the class of random walk

---
+++ random_walk.py
@@ -0,0 +1,34 @@
+from random import choice
+
+class RandomWalk():
+    """A class to generate random walk"""
+
+    def __init__(self, num_points=5000):
+        """Initialized attribute of walk"""
+        self.num_pionts = num_points
+
+        #All walks start at (0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        """Calculate all the points in the walk"""
+
+        while len(self.x_values) < self.num_pionts:
+
+            x_direction = choice([-1, 1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([-1, 1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            if x_step == 0 and y_step == 0:
+                continue
+
+            next_x = self.x_values[-1] + x_step
+            next_y = self.y_values[-1] + y_step
+
+            self.x_values.append(next_x)
+            self.y_values.append(next_y)

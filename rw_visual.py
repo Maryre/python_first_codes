1e7b6ae89a6599c0bd1458a3077b3ad3f0106329
Maryre <en.mrezaei@gmail.com>
2018-12-08 19:11:42 +0330

    plotting a loop of random walk

--- rw_visual.py
+++ rw_visual.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.scatter(rw.x_values, rw.y_values, c='green', s=15)
+    plt.show()
+
+    running = input("Make another walk? y/n: ")
+    if running == 'n':
+        break

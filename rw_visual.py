8ef99f439009e7b0e057fe931d114528404496f9
Maryre <en.mrezaei@gmail.com>
2018-12-09 15:03:36 +0330

    plotting the start and end point

--- rw_visual.py
+++ rw_visual.py
@@ -9,6 +9,9 @@ while True:
 
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
                 cmap=plt.cm.Blues, edgecolor='none', s=15)
+    #emphsize the first and last points
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
     plt.show()
 
     running = input("Make another walk? y/n: ")

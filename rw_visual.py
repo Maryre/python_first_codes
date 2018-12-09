09db5af40a2da5d6c2936b3ca2ba00d944f4d514
Maryre <en.mrezaei@gmail.com>
2018-12-09 15:07:03 +0330

    remove the axes

--- rw_visual.py
+++ rw_visual.py
@@ -12,6 +12,10 @@ while True:
     #emphsize the first and last points
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    #remove the axes
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
 
     running = input("Make another walk? y/n: ")

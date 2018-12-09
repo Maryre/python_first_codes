6d012bdcd5b668cf144740365458cae37afe370c
Maryre <en.mrezaei@gmail.com>
2018-12-09 15:10:01 +0330

    increase the random walk number

--- rw_visual.py
+++ rw_visual.py
@@ -2,20 +2,20 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     point_numbers = list(range(rw.num_pionts))
 
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
-                cmap=plt.cm.Blues, edgecolor='none', s=15)
+                cmap=plt.cm.Blues, edgecolor='none', s=1)
     #emphsize the first and last points
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
 
     #remove the axes
-    plt.axes().get_xaxis().set_visible(False)
-    plt.axes().get_yaxis().set_visible(False)
+    # plt.axes().get_xaxis().set_visible(False)
+    # plt.axes().get_yaxis().set_visible(False)
     plt.show()
 
     running = input("Make another walk? y/n: ")

e02bca47515ba6a42e7ca201c772db6988cbfc05
Maryre <en.mrezaei@gmail.com>
2018-12-11 22:11:02 +0330

    ploting high and low temperature with dates (year 2014)

--- lows_highs.py
+++ lows_highs.py
@@ -13,12 +13,14 @@ with open(filename) as f:
     #    print(index, column_header)
 
     #get date and high temprature from file
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
         high = int(row[1])
         highs.append(high)
+        low = int(row[3])
+        lows.append(low)
 
     print(highs)
 
@@ -26,9 +28,10 @@ with open(filename) as f:
 
     fig = plt.figure(dpi=128, figsize=(10,6))
     plt.plot(dates, highs, c='red')
+    plt.plot(dates, lows, c='blue')
 
     #plot format
-    plt.title("Daily high temperature, 2014", fontsize=24)
+    plt.title("Daily high and low temperature, 2014", fontsize=24)
     fig.autofmt_xdate()
     plt.xlabel('', fontsize=16)
     plt.ylabel("Temperature (F)", fontsize=16)

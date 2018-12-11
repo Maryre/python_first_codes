55e05724692085634281bd832111e03e68f0b909
Maryre <en.mrezaei@gmail.com>
2018-12-11 21:46:00 +0330

    reading the index and header row then we print the high temprature

---
+++ lows_highs.py
@@ -0,0 +1,18 @@
+import csv
+
+filename = 'sitka_weather_07-2014.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
+
+    # for index, column_header in enumerate(header_row):
+    #    print(index, column_header)
+
+    #get high temprature from file
+    highs = []
+    for row in reader:
+        highs.append(row[1])
+
+    print(highs)
